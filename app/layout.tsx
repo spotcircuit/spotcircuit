15d0ba0c27e48c2da21e18bda546865e8f390ac9
spotcircuit <info@spotcircuit.com>
2025-04-11 11:16:52 -0400

    contact form fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   )
 }
--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -14,10 +14,6 @@ export default function ServicesLayout({
   return (
     <>
       {children}
-      <Script
-        src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-        strategy="afterInteractive"
-      />
     </>
   );
 }
